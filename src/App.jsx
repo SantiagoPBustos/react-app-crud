79d45253162ba5ec072154beaa320bdd3eb12a90
SantiagoPBustos <david.pina@uptc.edu.co>
2023-02-21 15:10:03 -0500

    first commit to upload project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-center mt-5'>Estamos trabajando...</h1>
+    </div>
+  )
+}
+
+export default App
