df2f8c983126e12f12dbe1474393e44bb49cd720
SantiagoPBustos <david.pina@uptc.edu.co>
2023-02-24 00:14:36 -0500

    ok but 4 days

--- src/components/FormCRUD.jsx
+++ src/components/FormCRUD.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
 
-const initialForm = {
+const initialForm={
   id: null,
   Tipo: "",
   Nombre: "",
